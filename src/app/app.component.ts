cbc821c20c743bf7b6ac492751cd025566f8fe5f
Divya <divyapalanisamy8@gmail.com>
2021-09-24 18:44:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayOfWords from '../utils/words'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  limit = 10;
+  RandomLimit = 0;
+  word='';
+  RandomWords = '';
+
+
+  sliderValue(e: any){
+    this.limit = e.target.value;
+  }
+
+  generateRandom(){
+    this.RandomLimit = Math.ceil( Math.random() * 10);
+    this.RandomWords = arrayOfWords.slice(0, this.RandomLimit).join(' ');
+  }
+
+  generateWords(){
+    this.word = arrayOfWords.slice(0,this.limit).join(' ');
+  }
+
+  
 }
